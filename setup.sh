93b23ef53636b1e95a51dabb15ad1d17eb716ca4
OMSmolina <omsmolina.world@gmail.com>
2022-09-01 21:35:12 +0200

    Added setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 # Config Git
+git config branch.main.rebase true
 
 # Copy Git Hooks
+cp -R tools/hooks/* .git/hooks/
 
-# Download Development Environment (Theme, Data packages)
+# Download Theme And Run Setup
+git clone git@github.com:OMSTest/omstest-theme.git themes/omstest-theme
+./themes/omstest-theme/setup.sh
 
-#hugo mod
+#Install NPM Packages
+npm install
