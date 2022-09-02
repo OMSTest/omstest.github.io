3b2b31feb1279eea674c92d3ec8e1e0740db945b
OMSmolina <omsmolina.world@gmail.com>
2022-09-02 17:09:45 +0200

    Added pull pr and pull devenv

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ git config branch.main.rebase true
 # Copy Git Hooks
 cp -R tools/hooks/* .git/hooks/
 
-# Download Theme
-git clone git@github.com:OMSTest/omstest-theme.git themes/omstest-theme
+# Download Development Environment
+./tools/pull-devenv
 
 # Install NPM Packages
 npm install
